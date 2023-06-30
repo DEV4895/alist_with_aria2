56dc7b48b92018196d002d59ffc97e3398ca9f75
唐梓航 <administrator@dev081205.onmicrosoft.com>
2023-06-30 04:39:11 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ umask ${UMASK}
 exec su-exec ${PUID}:${PGID} nohup aria2c \
   --enable-rpc \
   --rpc-allow-origin-all \
-  --conf-path=/root/.aria2/aria2.conf >/root/aria2.out 2>&1 &
+  --conf-path=/root/.aria2/aria2.conf >/root/.aria2/aria2.out 2>&1 &
 
 exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
